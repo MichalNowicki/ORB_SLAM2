1bcc6de3df6f6d88defaa1f9cd4fd62637f93b9a
Michal Nowicki <michalsminowicki@gmail.com>
2017-06-25 00:37:31 +0200

    Testing script to run ORBSLAM2

---
+++ scripts/orbslamRun.py
@@ -0,0 +1,88 @@
+#
+#	author: Michal Nowicki
+#
+from subprocess import call
+import subprocess
+import sys
+import glob
+import os
+
+# Path to save main results - create if needed
+if not os.path.exists("results"):
+	os.makedirs("results");	
+else:
+	call('rm results/*', shell=True);
+
+
+sequences = [
+'01', \
+'02', \
+'03', \
+'04', \
+'05', \
+'06', \
+'07', \
+'08', \
+'09', \
+'10', \
+'11', \
+'12', \
+'13', \
+'14', \
+'15', \
+'16', \
+'17', \
+'18', \
+'19', \
+'20', \
+'21', \
+'22', \
+'23', \
+'24', \
+'25', \
+'26', \
+'27', \
+'28', \
+'29', \
+'30', \
+'31', \
+'32', \
+'33', \
+'34', \
+'35', \
+'36', \
+'37', \
+'38', \
+'39', \
+'40', \
+'41', \
+'42', \
+'43', \
+'44', \
+'45', \
+'46', \
+'47', \
+'48', \
+'49', \
+'50'];
+
+runsPerSequence = 1;
+
+
+mainDatasetPath = '/home/michal/dsoDataset';
+
+
+for seq in sequences:
+
+
+	for runId in range(0, runsPerSequence):
+		print("Current sequence: " + seq);
+
+		print('./Examples/Monocular/mono_tum_dso Vocabulary/ORBvoc.txt Examples/Monocular/TUM_MONO_DSO.yaml ' + str(mainDatasetPath) +'/' + seq +'/');
+
+		# Copy to currently used settings
+		call('./Examples/Monocular/mono_tum_dso Vocabulary/ORBvoc.txt Examples/Monocular/TUM_MONO_DSO.yaml ' + str(mainDatasetPath) +'/' + seq +'/', shell=True);
+		
+		# Run software
+		call('mv KeyFrameTrajectory.txt results/sequence_' + str(seq) + '_' + str(runId) + '.txt', shell=True);
+
