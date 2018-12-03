ff33b4d8afbae302521ca9c035d3f47ffc2ce670
Michal Nowicki <michal.nowicki@put.poznan.pl>
2018-12-03 01:28:49 +0100

    1500 features in LBA

--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -460,12 +460,12 @@ namespace ORB_SLAM2 {
 //        std::cout << "Optimizer::FeatureOptimization - end" << std::endl;
 
         // TODO: Simple selection based on the number of obs
-        if (lLocalMapPoints.size() > 250) {
+        if (lLocalMapPoints.size() > 1500) {
             lLocalMapPoints.sort([](const MapPoint *a, const MapPoint *b) {
                 return a->nObs > b->nObs;
             });
-            std::cout << "Resize performed! From " << lLocalMapPoints.size() << " to 250" << std::endl;
-            lLocalMapPoints.resize(250);
+            std::cout << "Resize performed! From " << lLocalMapPoints.size() << " to 1500" << std::endl;
+            lLocalMapPoints.resize(1500);
         }
 
 
